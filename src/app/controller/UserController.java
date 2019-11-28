481d115d4986b28d5c869de2343b1f58566077aa
Paweł <pjanic98@gmail.com>
2019-11-28 03:40:19 +0100

    Add league adding to system

--- src/app/controller/UserController.java
+++ src/app/controller/UserController.java
@@ -4,6 +4,10 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.github.javafaker.Faker;
 
+import app.entities.League;
+import app.entities.Match;
 import app.entities.User;
 import app.service.UserService;
 
@@ -26,7 +32,12 @@ public class UserController {
 	@GetMapping("/list")
 	public String listCustomers(Model model)
 	{
-		List<User> users = userService.getUsers();
+//		List<User> users = userService.getUsers();
+//		
+//		model.addAttribute("users", users);
+//		
+//		return "list-users";
+		List<League> users = userService.getLeagues();
 		
 		model.addAttribute("users", users);
 		
@@ -36,26 +47,65 @@ public class UserController {
 	@PostMapping("/list")
 	public String saveUsers()
 	{
-		String[] roleList = new String[3];
-		roleList[0] = "admin";
-		roleList[1] = "administrative";
-		roleList[2] = "physical";
+		String[] names = {"Premier League2", "Bundesliga2", "Serie A2", "La Liga2", "Ligue 12"};
+		String[] nationalities = {"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla",
+	            "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas",
+	            "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
+	            "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam",
+	            "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands",
+	            "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia",
+	            "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire",
+	            "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
+	            "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia",
+	            "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana",
+	            "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar",
+	            "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti",
+	            "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India",
+	            "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan",
+	            "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait",
+	            "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya",
+	            "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar",
+	            "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius",
+	            "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat",
+	            "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles",
+	            "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands",
+	            "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn",
+	            "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda",
+	            "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino",
+	            "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore",
+	            "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa",
+	            "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon",
+	            "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic",
+	            "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga",
+	            "Trinidad and Tobago", "Tunisia", "Türkiye", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine",
+	            "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay",
+	            "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)",
+	            "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"};
 		
 		Random generator = new Random();
 		Faker faker = new Faker();
 		
-		for(int i=0; i<40; i++)
+		for(int i=0; i<names.length; i++)
 		{
-			String login = faker.name().username();
-			String password = faker.code().asin();
-			Date date = null;
-			String name = faker.name().firstName();
-			String surname = faker.name().lastName();
-			String address = faker.address().fullAddress();
+			String name = names[i];
+			String nationality = nationalities[generator.nextInt(nationalities.length)];
+			int level = generator.nextInt(4);
+			int win_pts = 3;
+			int draw_pts = 1;
 			
-			User user = new User(login, password, date, name, surname, roleList[generator.nextInt(3)], address);
+			League league = new League(name, level, win_pts, draw_pts);
+			userService.saveLeague(league);
 			
-			userService.saveUser(user);
+//			String login = faker.name().username();
+//			String password = faker.code().asin();
+//			Date date = null;
+//			String name = faker.name().firstName();
+//			String surname = faker.name().lastName();
+//			String address = faker.address().fullAddress();
+//			
+//			User user = new User(login, password, date, name, surname, roleList[generator.nextInt(3)], address);
+//			
+//			userService.saveUser(user);
 		}
 		return "redirect:/user/list";
 	}
--- src/app/service/UserService.java
+++ src/app/service/UserService.java
@@ -2,11 +2,16 @@ package app.service;
 
 import java.util.List;
 
+import app.entities.League;
 import app.entities.User;
 
 public interface UserService {
 	
 	public List<User> getUsers();
+	
+	public List<League> getLeagues();
 
 	public void saveUser(User user);
+	
+	public void saveLeague(League league);
 }
